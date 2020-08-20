229682cb897b8a5badb50e6e38394213249106a5
Amit Sharma <48654350+amitsharma123234@users.noreply.github.com>
2020-08-20 09:32:09 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -12,8 +12,10 @@ if Var.STRING_SESSION:
     session_name = str(Var.STRING_SESSION)
     bot = TelegramClient(StringSession(session_name), Var.APP_ID, Var.API_HASH)
 else:
-    session_name = "startup"
-    bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
+    quit(1)
+if Var.STRING2
+    session_name = str(Var.STRING2)
+    bot2 = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
 
 CMD_LIST = {}
 CMD_HELP = {}
