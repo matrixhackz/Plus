cb1bba183bf2fda6f048b695540452d4ccd2a1b1
Amit Sharma <48654350+amitsharma123234@users.noreply.github.com>
2020-08-20 14:13:11 +0530

    Why to use more lines🙄

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -9,13 +9,11 @@ UpTime = time.time()
 os.system("pip install --upgrade pip")
 
 if Var.STRING_SESSION:
-    session_name = str(Var.STRING_SESSION)
-    bot = TelegramClient(StringSession(session_name), Var.APP_ID, Var.API_HASH)
+    bot = TelegramClient(StringSession(Var.STRING_SESSION), Var.APP_ID, Var.API_HASH)
 else:
     quit(1)
 if Var.STRING2:
-    session_name = str(Var.STRING2)
-    bot2 = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
+    bot2 = TelegramClient(StringSession(Var.STRING2), Var.APP_ID, Var.API_HASH)
 
 CMD_LIST = {}
 CMD_HELP = {}
